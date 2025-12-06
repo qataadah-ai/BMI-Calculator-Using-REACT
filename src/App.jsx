3967f74b9765173d0544684cf8efb632a501143a
Qataadah <qaxiafridi@gmail.com>
2025-12-06 23:58:17 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+const [height, setheight] = useState("")
+const [weight, setweight]= useState("")
+const [BMI , setBMI]= useState("")
+
+const calculateBmi = ()=>{
+
+  if(height>0 && weight >0){
+   const  heightinCM = height/100
+  const result = (weight / (heightinCM*heightinCM)).toFixed(2);
+  setBMI(result)
+  }else{
+    setBMI("invalid BMI")
+  }
+
+}
+
+
+  return (
+    <div className='Section'>
+    <div className='BMI-contain'>
+      <h2>BMI Calculator</h2>
+        
+      
+      <input type="number" 
+      placeholder='Enter Height in Cm'
+      value={height}
+      onChange={(e)=>setheight(e.target.value)}
+      />
+
+       <input type="number" 
+      placeholder='Enter Weight'
+      value={weight}
+      onChange={(e)=>setweight(e.target.value)}
+      />
+
+      <button onClick={calculateBmi}>calculate</button>
+        {BMI && (
+           <h4>Your BMI IS {BMI}</h4>
+       
+        )}
+     
+
+  {BMI && (
+  <p>
+ 
+  {BMI < 18.5
+    ? " Underweight"
+    : BMI < 24.9
+    ? " Normal Weight"
+    : BMI < 29.9
+    ? " Overweight"
+    : ""}
+</p>
+)}
+
+
+ 
+    </div>
+    
+    </div>
+  )
+}
+
+export default App
+     
